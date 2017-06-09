aef7099698b123acf8fb12ee0854dc61df8e1c7e
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-06-09 16:48:21 +0530

    Added all main functionalities

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns=[
 	url(r'^$', views.post_list, name='post_list'),
+	url(r'^post/all/$', views.post_all, name='post_all'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^drafts/$', views.post_draft, name='post_draft'),
+	url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+	url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 ]
